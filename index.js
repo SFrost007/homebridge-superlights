284db59fadd856611ce7faeab127dfe5a963df77
Simon Frost <sfrost007@gmail.com>
2016-11-24 03:23:42 +0000

    Make tabs vs spaces consistent in sourced code

--- index.js
+++ index.js
@@ -221,92 +221,92 @@ SuperlightAccessory.prototype.writeToBulb = function(callback) {
 
 // From http://stackoverflow.com/questions/8022885/rgb-to-hsv-color-in-javascript
 SuperlightAccessory.prototype.rgb2hsv = function(r, g, b) {
-  var rr, gg, bb,
-      r = r / 255,
-      g = g / 255,
-      b = b / 255,
-      h, s,
-      v = Math.max(r, g, b),
-      diff = v - Math.min(r, g, b),
-      diffc = function(c){
-          return (v - c) / 6 / diff + 1 / 2;
-      };
+	var rr, gg, bb,
+			r = r / 255,
+			g = g / 255,
+			b = b / 255,
+			h, s,
+			v = Math.max(r, g, b),
+			diff = v - Math.min(r, g, b),
+			diffc = function(c){
+					return (v - c) / 6 / diff + 1 / 2;
+			};
 
-  if (diff == 0) {
-      h = s = 0;
-  } else {
-      s = diff / v;
-      rr = diffc(r);
-      gg = diffc(g);
-      bb = diffc(b);
+	if (diff == 0) {
+			h = s = 0;
+	} else {
+			s = diff / v;
+			rr = diffc(r);
+			gg = diffc(g);
+			bb = diffc(b);
 
-      if (r === v) {
-          h = bb - gg;
-      }else if (g === v) {
-          h = (1 / 3) + rr - bb;
-      }else if (b === v) {
-          h = (2 / 3) + gg - rr;
-      }
-      if (h < 0) {
-          h += 1;
-      }else if (h > 1) {
-          h -= 1;
-      }
-  }
-  return {
-      h: Math.round(h * 360),
-      s: Math.round(s * 100),
-      v: Math.round(v * 100)
-  };
+			if (r === v) {
+					h = bb - gg;
+			}else if (g === v) {
+					h = (1 / 3) + rr - bb;
+			}else if (b === v) {
+					h = (2 / 3) + gg - rr;
+			}
+			if (h < 0) {
+					h += 1;
+			}else if (h > 1) {
+					h -= 1;
+			}
+	}
+	return {
+			h: Math.round(h * 360),
+			s: Math.round(s * 100),
+			v: Math.round(v * 100)
+	};
 }
 
 // From https://gist.github.com/eyecatchup/9536706
 SuperlightAccessory.prototype.hsv2rgb = function(h, s, v) {
-    var r, g, b;
-    var i;
-    var f, p, q, t;
-     
-    // Make sure our arguments stay in-range
-    h = Math.max(0, Math.min(360, h));
-    s = Math.max(0, Math.min(100, s));
-    v = Math.max(0, Math.min(100, v));
-     
-    // We accept saturation and value arguments from 0 to 100 because that's
-    // how Photoshop represents those values. Internally, however, the
-    // saturation and value are calculated from a range of 0 to 1. We make
-    // That conversion here.
-    s /= 100;
-    v /= 100;
-     
-    if(s == 0) {
-        // Achromatic (grey)
-        r = g = b = v;
-        return {
-            r: Math.round(r * 255), 
-            g: Math.round(g * 255), 
-            b: Math.round(b * 255)
-        };
-    }
-     
-    h /= 60; // sector 0 to 5
-    i = Math.floor(h);
-    f = h - i; // factorial part of h
-    p = v * (1 - s);
-    q = v * (1 - s * f);
-    t = v * (1 - s * (1 - f));
-     
-    switch(i) {
-        case 0: r = v; g = t; b = p; break;
-        case 1: r = q; g = v; b = p; break;
-        case 2: r = p; g = v; b = t; break;
-        case 3: r = p; g = q; b = v; break;
-        case 4: r = t; g = p; b = v; break;
-        default: r = v; g = p; b = q;
-    }
-     
-    return {
-        r: Math.round(r * 255), 
-        g: Math.round(g * 255), 
-        b: Math.round(b * 255)
-    };
+		var r, g, b;
+		var i;
+		var f, p, q, t;
+		 
+		// Make sure our arguments stay in-range
+		h = Math.max(0, Math.min(360, h));
+		s = Math.max(0, Math.min(100, s));
+		v = Math.max(0, Math.min(100, v));
+		 
+		// We accept saturation and value arguments from 0 to 100 because that's
+		// how Photoshop represents those values. Internally, however, the
+		// saturation and value are calculated from a range of 0 to 1. We make
+		// That conversion here.
+		s /= 100;
+		v /= 100;
+		 
+		if(s == 0) {
+				// Achromatic (grey)
+				r = g = b = v;
+				return {
+						r: Math.round(r * 255), 
+						g: Math.round(g * 255), 
+						b: Math.round(b * 255)
+				};
+		}
+		 
+		h /= 60; // sector 0 to 5
+		i = Math.floor(h);
+		f = h - i; // factorial part of h
+		p = v * (1 - s);
+		q = v * (1 - s * f);
+		t = v * (1 - s * (1 - f));
+		 
+		switch(i) {
+				case 0: r = v; g = t; b = p; break;
+				case 1: r = q; g = v; b = p; break;
+				case 2: r = p; g = v; b = t; break;
+				case 3: r = p; g = q; b = v; break;
+				case 4: r = t; g = p; b = v; break;
+				default: r = v; g = p; b = q;
+		}
+		 
+		return {
+				r: Math.round(r * 255), 
+				g: Math.round(g * 255), 
+				b: Math.round(b * 255)
+		};
 }
